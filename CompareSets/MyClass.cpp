38ea79bcb1b6a4aec99e7875032fe8f3143ae706
Sergey Evseev <sevseev@gmail.com>
2022-06-02 22:01:52 +0300

    Add project files.

---
+++ CompareSets/MyClass.cpp
@@ -0,0 +1,134 @@
+#include "MyClass.h"
+#include <iostream> 
+using namespace std;
+
+//конструктор по умолчанию // создает массив на 10 элементов //
+MyClass::MyClass()
+{
+	cout << "\nКонструктор по умолч. - отсортированный массив на 10 элементов, объект " << this << ": ";
+	int Size{ 10 };
+	this->data = new int[Size]; //int arr[10];
+	for (int i = 0; i < Size; i++) //заполняем массив рандомными значениями
+	{
+		data[i] = rand() % 100;
+	}
+	PrintArr(data, Size); cout << "- массив до сортировки" << endl;
+	MyClass::quickSort(data, (Size - 1));//вызов быстрой сортировки
+	PrintArr(data, Size);//вывод отсортированного массива
+	cout << "- массив после сортировки" << endl;
+}
+
+//Конструктор создания массива заданного размера//
+MyClass::MyClass(int size)//передаем размерность массива
+{
+	cout << "\nМассив на " << size << " элементов, создан объект " << this << " :" << endl;
+	this->Size = size;
+	this->data = new int[size];//создаем дин.массив и присваиваем его переменной-указателю класса 
+	for (int i = 0; i < size; i++) //заполняем массив рандомными значениями
+	{
+		*(data + i) = rand() % 100;
+	}
+	PrintArr(data, Size);
+}
+//перегрузка конструктора копирования//
+MyClass::MyClass(const MyClass& other)
+{
+	cout << "\nКонструктор копирования, создан объект " << this << ": " << endl;
+	//this->data = other.data; реализация конструктора копирования по умолчанию
+	this->Size = other.Size;
+	this->data = new int[other.Size]; //наша реализация конструктора копирования 
+	for (int i = 0; i < other.Size; i++)
+	{
+		this->data[i] = other.data[i];
+	}
+	PrintArr(data, Size); cout << endl;
+}
+//перегрузка оператора присваивания//
+MyClass& MyClass::operator=(const MyClass& other)
+{
+	cout << "Оператор присваивания, создан объект: " << this << endl;
+	this->Size = other.Size;//поле Size просто копируем, так как он в динамической памяти не выделяется (очищать не нужно)
+	if (this->data != nullptr)//проверка на ненулевой указатель
+	{
+		delete[] this->data;
+	}
+	//логика поэлементного копирования после очистки текущего объекта:
+	this->data = new int[other.Size];
+	for (int i = 0; i < other.Size; i++)
+	{
+		this->data[i] = other.data[i];
+	}
+	PrintArr(data, Size); cout << endl;
+	return *this;
+}
+
+//функция вывода PrintArr//
+void MyClass::PrintArr(int* arr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << *(arr + i) << " ";
+	}
+	cout << endl;
+}
+//функция быстрой сортировки для конструктора по умолчанию//
+void MyClass::quickSort(int* data, int N) //data[N]-послед. эл. массива
+{
+	int i = 0, j = N; int temp, p;
+	p = data[N / 2];
+	do {
+		while (data[i] < p) i++;
+		while (data[j] > p) j--;
+		if (i <= j) {
+			temp = data[i]; data[i] = data[j]; data[j] = temp;
+			i++; j--;
+		}
+	} while (i <= j);
+	if (j > 0) quickSort(data, j);
+	if (N > i) quickSort(data + i, N - i);
+}
+//функция проверки числа в массиве
+bool MyClass::isInclude(int d)
+{
+	cout << "Поиск числа: " << d << " в массиве " << this;
+	if (data != nullptr) {
+		for (int i = 0; i < Size; i++) {
+			if (d == getNum(i)) {
+				cout << " - есть такое число!" << endl << endl;
+				return true;
+			}
+		}
+	}
+	cout << " - нет такого числа!" << endl << endl;
+	return false;
+}
+int MyClass::getNum(int i)//функция получения значения элемента
+{
+	return this->data[i];
+}
+
+//функция добавления элемента в массив//
+MyClass& MyClass::add(int i)
+{
+	if (!isInclude(i)) {//если такого числа нет в массиве
+		int* temp = new int[this->Size + 1];//созд.врем. массива на 1 эл-т больше
+		for (int j = 0; j < Size; j++) { //копирование эл-тов в врем. массив
+			temp[j] = data[j];
+		}
+		temp[Size] = i;//последнему элементу присвоить передаваемое значение
+		delete[] data;//удалить элементы старого массива
+		data = temp;//присвоить массиву объекта новый массив 
+		Size++;
+	}
+	cout << "Массив " << this << " после добавления числа " << i << ":" << endl;
+	PrintArr(data, Size); cout << endl;
+	return *this;
+}
+
+//Деструктор объекта и массива//
+MyClass::~MyClass()
+{
+	cout << "Деструктор объекта " << this << endl;
+	delete[] data; //в деструкторе также уничтожаем сам массив, чтобы не было утечек памяти
+}
+
---
+++ CompareSets/MyClass.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class MyClass
+{
+	int* data;//создаем переменную указатель для массива
+private:
+	int Size;
+public:
+	MyClass();//конструктор по умолчанию
+	MyClass(int size); //конструктор массива заданного размера
+	MyClass(const MyClass& other);//конструктор копирования
+	MyClass& operator = (const MyClass& other);//перегруженный оператор присваивания
+	void PrintArr(int* arr, int size);//вывод массива	
+	void quickSort(int* arr, int N);//функция быстрой сортировки
+	bool isInclude(int d);//функция проверки наличия числа
+	int getNum(int i);//функция возвращает значение i-го элемента массива
+	MyClass& add(int i); //добавление числа в массив
+
+	~MyClass();
+};
+
